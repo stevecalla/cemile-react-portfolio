d1f76dc3bcc0d2af59bd3cff2b9b1b3691f78b0c
Cemileb <cemilebalkas@gmail.com>
2024-01-31 17:53:35 +0000

    installed vite bootstrap and react router

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+import './App.css'
+
+function App() {
+  
+
+  return 
+}
+
+export default App
