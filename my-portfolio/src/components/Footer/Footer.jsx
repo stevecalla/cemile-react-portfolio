1977e0839c16513f536acf590c869fcd2d3bc7f9
Steve Calla <callasteven@gmail.com>
2024-02-02 15:48:01 -0700

    Update

--- my-portfolio/src/components/Footer/Footer.jsx
+++ my-portfolio/src/components/Footer/Footer.jsx
@@ -1,37 +1,68 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+// import {
+//   faLinkedin,
+//   faGithub,
+//   faTwitter,
+// } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    return (
-        <div className='social-media-wrapper'>
-            <footer className="text-center text-lg-startd-flex justify-content-evenly align-items-center pt-5">
-            <div className="mb-3">
+  return (
+    <div className="social-media-wrapper">
+      <footer className="text-center text-lg-startd-flex justify-content-evenly align-items-center pt-5">
+        <div className="mb-3">
+          <a
+            href="https://github.com/cemileblks"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Link to GitHub"
+            className="text-pink mx-3"
+          >
+            <i className="fab fa-github fa-2x"></i>
+            {/* <FontAwesomeIcon icon={faGithub} size="xl" /> */}
+          </a>
 
-                        <a href="https://github.com/cemileblks" target="_blank" rel="noopener noreferrer" aria-label="Link to GitHub" className="text-pink mx-3">
-                            <i className="fab fa-github fa-2x"></i>
-                        </a>
+          <a
+            href="https://www.linkedin.com/in/cemile-b-606084146/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Link to LinkedIn"
+            className="text-pink mx-3"
+          >
+            <i className="fab fa-linkedin fa-2x"></i>
+            {/* <FontAwesomeIcon icon={faLinkedin} size="xl" /> */}
+          </a>
 
+          <a
+            href="mailto:cemilebalkas@gmail.com"
+            aria-label="Send an Email"
+            className="text-pink mx-3"
+          >
+            <i className="fas fa-envelope fa-2x"></i>
+            {/* <FontAwesomeIcon icon={faEnvelope} size="xl" /> */}
+          </a>
 
-                        <a href="https://www.linkedin.com/in/cemile-b-606084146/" target="_blank" rel="noopener noreferrer" aria-label="Link to LinkedIn" className="text-pink mx-3">
-                            <i className="fab fa-linkedin fa-2x"></i>
-                        </a>
-
-                        <a href="mailto:cemilebalkas@gmail.com" aria-label="Send an Email" className="text-pink mx-3">
-                            <i className="fas fa-envelope fa-2x"></i>
-                        </a>
-
-                        <a href="https://twitter.com/Cemile_blks" aria-label="Send an Email" className="text-pink mx-3">
-                        <i className="fa-brands fa-x-twitter fa-2x"></i>
-                        </a>
-
-                        </div>
-
-
-                <div className="text-center text-white p-3 footer-text">
-                    <a className="text-white" href="https://github.com/cemileblks">© 2024 by CEMILE BALKAS</a>
-                </div>
-            </footer>
+          <a
+            href="https://twitter.com/Cemile_blks"
+            aria-label="Send an Email"
+            className="text-pink mx-3"
+          >
+            <i className="fa-brands fa-x-twitter fa-2x"></i>
+            {/* <FontAwesomeIcon icon={faTwitter} size="xl" /> */}
+          </a>
         </div>
-    );
+
+        <div className="text-center text-white p-3 footer-text">
+          <a className="text-white" href="https://github.com/cemileblks">
+            © 2024 by CEMILE BALKAS
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 export default Footer;
--- my-portfolio/src/components/Home/Home.jsx
+++ my-portfolio/src/components/Home/Home.jsx
@@ -1,60 +1,66 @@
-import React from 'react';
-import Skills from '../Skills/Skills';
-import './Home.css'
+import React from "react";
+import Skills from "../Skills/Skills";
+import "./Home.css";
 
 function Home() {
-    const name = "CEMILE BALKAS"
-    return (
-        <div>
-            <div className="p-5 mb-4 bg-body-tertiary rounded-3 hero-banner">
-                <div className="container-fluid py-5 headline">
-                    <div className="row align-items-center">
-                        <div className="col-md-8 col-sm-12 mx-auto text-center text-md-start">
-                            <p className="fs-4">Hi, my name is </p>
-                            <h1 className="display-5 fw-bold">CEMILE BALKAS.</h1>
-                            <p className="fs-4 mt-3">
-                                I am a front-end web developer with a
-                                bioengineering background. My passion for technology and
-                                creativity drives me to design and develop unique websites.
-                                Combining my technical skills and innovative mindset, I strive to create visually appealing web pages.
-                            </p>
-                            <a href="https://cemileblks.github.io/react-portfolio/projects" className="btn button-pink mt-3 fs-5" rel="noopener noreferrer">
-                                View Projects
-                            </a>
-                            <a href="https://cemileblks.github.io/react-portfolio/contact" className="btn button-pink mt-3 fs-5 mx-3" rel="noopener noreferrer">
-                                Contact me
-                            </a>
-                        </div>
-                        <div className="col-md-4 col-sm-12 order-md-2 mt-4 mt-md-1">
-                            <div className="image-container rounded-circle">
-                                <img
-                                    src="./assets/images/cemile.png"
-                                    alt="Cemile Balkas image"
-                                    className="img-fluid rounded-circle"
-                                />
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+  const name = "CEMILE BALKAS";
+  return (
+    <div>
+      <div className="p-5 mb-4 bg-body-tertiary rounded-3 hero-banner">
+        <div className="container-fluid py-5 headline">
+          <div className="row align-items-center">
+            <div className="col-md-8 col-sm-12 mx-auto text-center text-md-start">
+              <p className="fs-4">Hi, my name is </p>
+              <h1 className="display-5 fw-bold">CEMILE BALKAS.</h1>
+              <p className="fs-4 mt-3">
+                I am a front-end web developer with a bioengineering background.
+                My passion for technology and creativity drives me to design and
+                develop unique websites. Combining my technical skills and
+                innovative mindset, I strive to create visually appealing web
+                pages.
+              </p>
+              <a
+                href="https://cemileblks.github.io/react-portfolio/projects"
+                className="btn button-pink mt-3 fs-5"
+                rel="noopener noreferrer"
+              >
+                View Projects
+              </a>
+              <a
+                href="https://cemileblks.github.io/react-portfolio/contact"
+                className="btn button-pink mt-3 fs-5 mx-3"
+                rel="noopener noreferrer"
+              >
+                Contact me
+              </a>
             </div>
-            < Skills />
+            <div className="col-md-4 col-sm-12 order-md-2 mt-4 mt-md-1">
+              <div className="image-container rounded-circle">
+                <img
+                  src="./assets/images/cemile.png"
+                  alt="Cemile Balkas image"
+                  className="img-fluid rounded-circle"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-    )
-    // <div>
-    //     <div className="p-5 mb-4 bg-body-tertiary rounded-3 hero-banner">
-    //         <div className="container-fluid py-5 headline">
-    //             <h1 className="display-5 fw-bold">CEMILE BALKAS</h1>
-    //             <p className="col-md-8 fs-4">Welcome to my portfolio! I am a front-end web developer with 
-    //             a bioengineering background. My passion for technology and creativity drives me to design 
-    //             and develop unique websites for different businesses and clients. Combining my technical 
-    //             skills and innovative mindset, I strive to create visually appealing and functional web pages.
-    //             </p>
-    //         </div>
-    //     </div>
-    // </div>
-
+      </div>
+      <Skills />
+    </div>
+  );
+  // <div>
+  //     <div className="p-5 mb-4 bg-body-tertiary rounded-3 hero-banner">
+  //         <div className="container-fluid py-5 headline">
+  //             <h1 className="display-5 fw-bold">CEMILE BALKAS</h1>
+  //             <p className="col-md-8 fs-4">Welcome to my portfolio! I am a front-end web developer with
+  //             a bioengineering background. My passion for technology and creativity drives me to design
+  //             and develop unique websites for different businesses and clients. Combining my technical
+  //             skills and innovative mindset, I strive to create visually appealing and functional web pages.
+  //             </p>
+  //         </div>
+  //     </div>
+  // </div>
 }
-
 
 export default Home;
