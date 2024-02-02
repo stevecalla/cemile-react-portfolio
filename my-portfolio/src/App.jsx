f8979ca1d02bd67c0af5f759396d9f967a461cb2
Steve Calla <callasteven@gmail.com>
2024-02-02 15:52:25 -0700

    Update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.css';
+
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import ProjectGallery from './components/ProjectGallery/ProjectGallery';
