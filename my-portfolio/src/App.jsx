224469e3654ffa4cbf46ebad9dca32b0ed488ac8
Steve Calla <callasteven@gmail.com>
2024-02-02 15:08:44 -0700

    Update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
