3ef5a72a1eeff08e96457ebb11f29e5b148ed68b
Steve Calla <callasteven@gmail.com>
2024-02-02 15:55:45 -0700

    Update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,7 +3,15 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
-import '@fortawesome/fontawesome-free/css/all.css';
+
+// import '@fortawesome/fontawesome-free/css/all.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
--- my-portfolio/src/components/Footer/Footer.jsx
+++ my-portfolio/src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Footer.css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 // import {
 //   faLinkedin,
 //   faGithub,
